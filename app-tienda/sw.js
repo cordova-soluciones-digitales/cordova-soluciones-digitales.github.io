5240240c168dc9783bce25fcc50ccb13acee8f9b
Cordova Soluciones Digitales <developer.cordova@gmail.com>
2026-08-05 21:05:17 -0600

    pwa

--- app-tienda/sw.js
+++ app-tienda/sw.js
@@ -14,7 +14,7 @@
  * - Limpia cachés de versiones anteriores en "activate".
  */
 
-const CACHE_VERSION = 'v5';
+const CACHE_VERSION = 'v6';
 const CACHE_NAME = `tienda-app-shell-${CACHE_VERSION}`;
 
 // App shell local
